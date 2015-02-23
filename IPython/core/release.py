e35e10170fb844bd00931c271d06fa15c25f6a5f
Didrik Pinte <dpinte@enthought.com>
2015-02-23 15:03:31 +0100

    update version number to follow PEP 386

--- IPython/core/release.py
+++ IPython/core/release.py
@@ -20,9 +20,9 @@ name = 'ipython'
 # release.  'dev' as a _version_extra string means this is a development
 # version
 _version_major = 2
-_version_minor = 4
+_version_minor = 3
 _version_patch = 0
-_version_extra = 'maint'
+_version_extra = 'maint0'
 # _version_extra = 'rc1'
 #_version_extra = ''  # Uncomment this for full releases
 
